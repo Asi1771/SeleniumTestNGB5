9a708d7a879f5c0a12190ef11cb7a913f85bfb46
asi1771 <asseldaribayeva@gmail.com>
2024-05-01 12:32:23 -0400

    Updated

--- src/test/java/tests/ConflictTests.java
+++ src/test/java/tests/ConflictTests.java
@@ -15,6 +15,6 @@ public class ConflictTests {
     @Test
     public void test2(){
         String message = "Welcome to USA!";
-        Assert.assertEquals("Welcome to USA!", message);
+        Assert.assertEquals("Welcome to US!", message);
     }
 }
